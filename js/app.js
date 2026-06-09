62f845a1b0249fb3c5ddc433ad90222d0902830f
zulhairy1558 <zulhairy1558@gmail.com>
2026-06-09 11:21:55 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -275,7 +275,7 @@ window.onload = function() {
     // ========== PWA Service Worker Registration ==========
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/sw.js')
+            navigator.serviceWorker.register('./sw.js')
                 .then((registration) => {
                     console.log('Service Worker registered with scope:', registration.scope);
                     
